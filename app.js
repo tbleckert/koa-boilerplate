4b4baaa11a42516c9b9865491a5ce41295f4c59d
Tobias Bleckert <tbleckert@gmail.com>
2014-05-11 21:47:57 +0200

    Logging

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var fs      = require('fs'),
     koa     = require('koa'),
     stylus  = require('koa-stylus'),
     serve   = require('koa-static'),
+    logger  = require('koa-logger'),
     favicon = require('koa-favi'),
     app     = koa(),
     config  = require('./app/config/app');
@@ -12,6 +13,10 @@ app.use(favicon());
 /** Stylus support */
 app.use(stylus('./public'));
 app.use(serve('./public'));
+
+if (process.env.NODE_ENV === 'development') {
+	app.use(logger());
+}
 
 require('./bootstrap.js')(app);
 
