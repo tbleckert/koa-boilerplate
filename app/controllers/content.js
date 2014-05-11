ef4cff4c25224fdfb26f15ebed370f1df783ed25
Tobias Bleckert <tbleckert@gmail.com>
2014-05-11 21:40:18 +0200

    Copy

--- app/controllers/content.js
+++ app/controllers/content.js
@@ -4,7 +4,7 @@ module.exports.enable = function (app, routes) {
 	
 	/** Index route */
 	app.use(route.get(routes.content.index, function *(){
-		yield this.render('index', {title: 'Hej'});
+		yield this.render('index', {title: 'Koa Boilerplate'});
 	}));
 
 };
